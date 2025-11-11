34490588218da15905d0cf3e6ff3145d6c9fd804
HarenKei <seungmin1266@gmail.com>
2025-11-11 20:53:59 +0900

    chore: MainSection.tsx에 온보딩 wrapper만 남김

---
+++ app/_components/ui/sections/MainSection.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import styled from "styled-components";
+
+export const OnBoardingPageWrapper = styled.main`
+  position: relative;
+  z-index: 5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+
+  max-height: 100vh;
+  max-height: 100dvh;
+  max-height: 100svh;
+`;
