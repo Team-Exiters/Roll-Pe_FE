4edf45953c84ec8cbb038d1bbb1a6e9fb04a913a
HarenKei <seungmin1266@gmail.com>
2025-12-03 09:33:10 +0900

    refactor: 마이페이지 서버 컴포넌트로 개선

--- app/(layout_HF)/mypage/my-rollpe/page.tsx
+++ app/(layout_HF)/mypage/my-rollpe/page.tsx
@@ -1,93 +1,20 @@
-"use client";
-import { COLORS } from "@/public/styles/colors";
-import styled from "styled-components";
-import { RollpeListItemProps } from "@/public/utils/types";
 import { RollpeList } from "@/app/_components/molecules/list/RollpeList";
-import { Rollpe } from "@/public/utils/types";
-import { useEffect, useState, useTransition } from "react";
-import { getUserRollpeList } from "@/public/utils/apis/rollpe";
-import Loading from "@/app/_components/molecules/ui/loading/Loading";
-import { ButtonMore } from "@/app/_components/molecules/ui/button/StyledButton";
-
-interface MyRollpeListData {
-  count: number;
-  next: string | null;
-  previous: string | null;
-  results: Rollpe[];
-}
+import {
+  MyRollpeWrapper,
+  MyRollpeContainer,
+} from "../_components/styles/InvitedRollpeStyles";
 
 const MyRollpePage: React.FC = () => {
-  const [isPending, startTransition] = useTransition();
-  const [myRollpeData, setMyRollpeData] = useState<MyRollpeListData>();
-  const [myRollpeList, setMyRollpeList] = useState<Rollpe[]>();
-
-  const getMyRollpeList = () => {
-    startTransition(async () => {
-      await getUserRollpeList("my")
-        .then((res) => {
-          console.log(res);
-          setMyRollpeData(res);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    });
-  };
-
-  useEffect(() => {
-    getMyRollpeList();
-  }, []);
-
-  useEffect(() => {
-    console.log(myRollpeData);
-    if (myRollpeData) {
-      setMyRollpeList(myRollpeData.results);
-    }
-  }, [myRollpeData]);
-
-  return isPending ? (
-    <Loading />
-  ) : (
+  return (
     <MyRollpeWrapper>
       <MyRollpeContainer>
         <div className={"title-wrapper"}>
           <h1>내 롤페</h1>
         </div>
-        {myRollpeList && (
-          <RollpeList
-            list={myRollpeList}
-            count={myRollpeData ? myRollpeData.count : 0}
-            resultText={""}
-          />
-        )}
-        <ButtonMore text="더보기" />
+        <RollpeList type={"my"} />
       </MyRollpeContainer>
     </MyRollpeWrapper>
   );
 };
-
-const MyRollpeWrapper = styled.main`
-  padding: 5rem 1.25rem;
-  width: calc(100% - 2.5rem);
-  font-family: var(--font-hakgyoansim);
-`;
-
-const MyRollpeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 2rem;
-
-  width: 100%;
-
-  & > .title-wrapper > h1 {
-    color: ${COLORS.ROLLPE_SECONDARY};
-    text-align: center;
-    font-size: 2rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-  }
-`;
 
 export default MyRollpePage;
